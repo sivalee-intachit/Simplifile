8678814f5b1b0286035947b699449c7bf0797db2
Thi <thiley12@yahoo.com>
2024-09-15 00:49:48 -0400

    working downloads fr this time

--- backend/app/services/extractData.py
+++ backend/app/services/extractData.py
@@ -1,3 +1,6 @@
+import os
+import requests
+import json
 from azure.storage.blob import BlobServiceClient
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from azure.core.credentials import AzureKeyCredential
@@ -6,7 +9,6 @@ import azure.cognitiveservices.speech as speechsdk
 # Azure Storage Blob credentials
 connection_string = "DefaultEndpointsProtocol=https;AccountName=vtpdfstorage;AccountKey=hz7qB4MVJEao2W0NFtpI7/9Nr4FKVLuGIyuYvxYqb+dqEHDKFq8M6TkEzJyXTxHivj5p7eUt1jfY+AStXR0now==;EndpointSuffix=core.windows.net"
 container_name = "pdfcontainer"
-blob_name = "vtpdfstorage"
 download_file_path = "/Users/thiley/Documents/GitHub/Simplifile/backend/downloaded.pdf"
 
 # Create a blob service client
@@ -15,7 +17,12 @@ blob_service_client = BlobServiceClient.from_connection_string(connection_string
 # Get the container client
 container_client = blob_service_client.get_container_client(container_name)
 
+# List blobs and get the most recent one
+blobs = list(container_client.list_blobs())
+most_recent_blob = max(blobs, key=lambda b: b['last_modified'])
+
 # Download the blob
+blob_name = most_recent_blob['name']
 with open(download_file_path, "wb") as download_file:
     blob_data = container_client.download_blob(blob_name)
     download_file.write(blob_data.readall())
